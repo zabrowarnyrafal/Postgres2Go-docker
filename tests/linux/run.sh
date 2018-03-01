1fe97e4c2e1023daedf3817747e3beb3c9cc19bd
Rafał Zabrowarny <zabrowarnyrafal@users.noreply.github.com>
2018-03-01 23:10:22 +0100

    Update run.sh

--- tests/linux/run.sh
+++ tests/linux/run.sh
@@ -3,7 +3,5 @@ set -eu -o pipefail
 
 dotnet add package Postgres2Go -s https://api.bintray.com/nuget/skyrise/Postgres2Go
 dotnet restore ./LinuxTests.csproj
-sudo useradd -ms /bin/bash postgres
-sudo groupadd postgres
 su postgres
 dotnet test ./LinuxTests.csproj
